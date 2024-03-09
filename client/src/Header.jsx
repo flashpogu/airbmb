24be242846874d7bc50966a5798b52e4eb80cda4
rahulkumar <107376311+flashpogu@users.noreply.github.com>
2024-03-09 20:28:42 +0530

    Update Header.jsx

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -5,30 +5,71 @@ import { UserContext } from "./UserContext";
 const Header = () => {
   const { user } = useContext(UserContext);
   return (
-    <header className="flex justify-between">
-      <Link to={"/"} href="" className="flex items-center gap-1">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6 -rotate-90"
+    <header className="flex justify-between max-md:flex-col max-md:items-center">
+      <div className="max-md:flex max-md:justify-between max-md:gap-14 flex justify-between gap-11">
+        <Link to={"/"} href="" className="flex items-center gap-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 -rotate-90"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+            />
+          </svg>
+          <span className="font-bold text-xl">airbmb</span>
+        </Link>
+        <Link
+          to={user ? "/account" : "/login"}
+          className="flex gap-2 border border-gray-300 rounded-full p-2 px-4 items-center"
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
-          />
-        </svg>
-        <span className="font-bold text-xl">airbmb</span>
-      </Link>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z"
+              clipRule="evenodd"
+            />
+          </svg>
+
+          <div className="bg-gray-500 text-white rounded-full border border-gray-500 overflow-hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                fillRule="evenodd"
+                d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+          {!!user && <div>{user.name}</div>}
+        </Link>
+      </div>
       <div className="flex gap-2 border border-gray-300 rounded-full p-2 px-4 shadow-md shadow-gray-200">
-        <div className="text-[16px] font-normal">Anywhere</div>
+        <div className="text-[16px] font-normal max-md:text-[14px]">
+          Anywhere
+        </div>
         <div className="border-l border-gray-300"></div>
-        <div className="text-[16px] font-normal">Any Week</div>
+        <div className="text-[16px] font-normal max-md:text-[14px]">
+          Any Week
+        </div>
         <div className="border-l border-gray-300"></div>
-        <div className="text-[16px] font-normal">Add guests</div>
+        <div className="text-[16px] font-normal max-md:text-[14px]">
+          Add guests
+        </div>
         <button className="bg-primary text-white rounded-full p-1">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -46,39 +87,6 @@ const Header = () => {
           </svg>
         </button>
       </div>
-      <Link
-        to={user ? "/account" : "/login"}
-        className="flex gap-2 border border-gray-300 rounded-full p-2 px-4 items-center"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            fillRule="evenodd"
-            d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z"
-            clipRule="evenodd"
-          />
-        </svg>
-
-        <div className="bg-gray-500 text-white rounded-full border border-gray-500 overflow-hidden">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              fillRule="evenodd"
-              d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </div>
-        {!!user && <div>{user.name}</div>}
-      </Link>
     </header>
   );
 };
